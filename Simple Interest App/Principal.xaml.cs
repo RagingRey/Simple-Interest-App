450e1e0688b48a9865ee3b4333db4f75d4ff8f0a
Daniel Beyioku <beyiokudaniel06@gmail.com>
2022-03-30 00:12:51 +0100

    Add project files.

---
+++ Simple Interest App/Principal.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Simple_Interest_App
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Principal : Page
+    {
+        public Principal()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            float time1 = float.Parse(time.Text);
+            float rate1 = float.Parse(rate.Text);
+            float interest1 = float.Parse(interest.Text);
+
+            float answer1 = (interest1 * 100) / (time1 * rate1);
+
+            answer.Text = answer1.ToString();
+        }
+    }
+}
---
+++ Simple Interest App/Time.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Simple_Interest_App
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Time : Page
+    {
+        public Time()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            float principal1 = float.Parse(principal.Text);
+            float rate1 = float.Parse(rate.Text);
+            float interest1 = float.Parse(interest.Text);
+
+            float answer1 = (interest1 * 100) / (principal1 * rate1);
+
+            answer.Text = answer1.ToString();
+        }
+    }
+}
